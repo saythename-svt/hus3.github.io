c6db508465a02913c2d32e09a0494267120cb912
Tori Haddox <tiredextras@gmail.com>
2025-09-24 19:08:02 -0500

    Saved username

--- javascript/external.js
+++ javascript/external.js
@@ -1,5 +1,7 @@
-arr = ["Mael","Eric","Randall","James"];
+/*arr = ["Mael","Eric","Randall","James"];
 for (var i=0; i < arr.length; i++)
 {
   document.writeln(arr[i],"<br>");
-}
+}*/
+user_name = window.prompt("Please enter your name", "Type your name here");
+document.write(user_name);
